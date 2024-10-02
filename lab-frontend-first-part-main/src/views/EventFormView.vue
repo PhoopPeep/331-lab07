c59c9d534f3ed0e76abb34d8be381ce547942b10
PhoopPeep <christina_c@cmu.ac.th>
2024-10-02 18:36:49 +0700

    lab09 6.6

--- lab-frontend-first-part-main/src/views/EventFormView.vue
+++ lab-frontend-first-part-main/src/views/EventFormView.vue
@@ -6,6 +6,7 @@ import OrganizerService from '@/services/OrganizerService';
 import { useRouter } from 'vue-router';
 import { useMessageStore } from '@/stores/message';
 import BaseInput from '@/components/BaseInput.vue';
+import BaseSelect from '@/components/BaseSelect.vue';
 
 const event = ref<Event>({
     id: 0,
@@ -77,16 +78,7 @@ onMounted(() => {
             <!-- Organizer -->
             <h3 class="text-lg font-semibold text-gray-700 mt-4">Who is your Organizer?</h3>
             <label>Select an Organizer</label>
-            <select v-model="event.organizer.id">
-                <option 
-                    v-for="option in organizers"
-                    :value="option.id"
-                    :key="option.id"
-                    :selected="option.id === event.organizer.id" 
-                >
-                    {{ option.name }}
-                </option>
-            </select>
+                <BaseSelect v-model="event.organizer.id" :options="organizers" label="Organizer" />
             <button type="submit" class="w-full bg-green-600 hover:bg-green-700 text-white py-3 rounded-md text-lg font-semibold mt-6">
                 Submit
             </button>
